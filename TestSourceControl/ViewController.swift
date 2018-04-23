8fd2254ad697d7649b10556ec6f92f57cab0cfc1
Matthieu <matthieupasserel@gmail.com>
2018-04-23 14:58:06 +0200

    Seconde branche

--- TestSourceControl/ViewController.swift
+++ TestSourceControl/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Je suis sur la seconde branche
     }
 
     override func didReceiveMemoryWarning() {
